60892a0c4a2a30e88fa9e187827c22d6338967d0
Piers Roberts <piers.roberts@somoglobal.com>
2016-08-18 14:32:33 +0100

    Added permissions to various options

--- controllers/StaticBlocks.php
+++ controllers/StaticBlocks.php
@@ -20,4 +20,18 @@ class Staticblocks extends \Backend\Classes\Controller
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
 
+    public $requiredPermissions = ['buzzwordcompliant.dbcontent.list_static_blocks'];
+
+    public function formExtendFields($form,$fields)
+    {
+        if(!$form->model->id)
+            return;
+        if(!$this->user->hasAccess('buzzwordcompliant.dbcontent.edit_static_block') || !$this->user->hasAccess('buzzwordcompliant.dbcontent.change_key_names')) {
+            foreach ($fields as $field) {
+                if ($field->fieldName == 'key') {
+                    $field->attributes['field']['readonly'] = true;
+                }
+            }
+        }
+    }
 }
